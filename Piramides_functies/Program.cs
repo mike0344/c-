632644db0fd261420fc6e18fb95335479ce4581a
mike0344 <58985414+mike0344@users.noreply.github.com>
2021-06-11 13:55:31 +0200

    Add files via upload

---
+++ Piramides_functies/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piramides_functies
+{
+    class Program
+    {
+        static void PrintLinksuitgelijndePiramide(int diepte, char symbool='*')
+        {
+            for (int i = 1; i <= diepte; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    System.Console.Write(symbool);
+                }
+                System.Console.WriteLine();
+            }
+        }
+
+        static void PrintRechtssuitgelijndePiramide(int diepte, char symbool = '*')
+        {
+            for (int i = 1; i <= diepte; i++)
+            {
+                for (int j = 1; j <= diepte; j++)
+                {
+                    if (j <= diepte-i)
+                    {
+                        System.Console.Write(' ');
+                    } else
+                    {
+                        System.Console.Write(symbool);
+                    }
+                }
+                System.Console.WriteLine();
+            }
+        }
+
+        static void PrintGecentreerdePiramide(int diepte, char symbool = '*')
+        {
+            for (int i = 1; i <= diepte; i++)
+            {
+                for (int j = 1; j < diepte+i; j++)
+                {
+                    if (j <= diepte - i)
+                    {
+                        System.Console.Write(' ');
+                    }
+                    else
+                    {
+                        System.Console.Write(symbool);
+                    }
+                }
+                System.Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            PrintLinksuitgelijndePiramide(5);
+            System.Console.WriteLine();
+            PrintLinksuitgelijndePiramide(5, '+');
+            System.Console.WriteLine();
+            PrintRechtssuitgelijndePiramide(5, '+');
+            System.Console.WriteLine();
+            PrintGecentreerdePiramide(10, '+');
+            Console.ReadLine();
+        }
+    }
+}
